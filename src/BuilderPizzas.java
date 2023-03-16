cba5431df0e94b545b8ce971295c523eacf21271
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-03-16 23:32:36 +0100

    hago javadoc

--- src/BuilderPizzas.java
+++ src/BuilderPizzas.java
@@ -1,76 +1,136 @@
 public class BuilderPizzas {
 
-    // este es el objeto que va a construir nuestro Builder
-    // utilizamos el guion bajo para remarcar que es un objeto de esta clase
-    protected Pizzas _pizza;
 
-    // metodo que devuelve el objeto terminado
+    /**
+     * Declaro objeto de tipo pizzas, el cual va a ser construido por el Builder
+     */
+    private Pizzas _pizza;
+
+    /**
+     * Metodo que devuelve el objeto terminado
+     * @return la pizza construida
+     */
     public Pizzas build() {
         return this._pizza;
     }
 
-    // crea la pizza para configurarla
+    /**
+     * Creo la pizza con la que voy a trabajar
+     */
     public BuilderPizzas() {
         _pizza = new Pizzas();
     }
 
-    // metodos para la personalizacion de la pizza
+    /**
+     * Metodo que devuelve el tipo de masa
+     * @param tipoMasa tipo de masa fina, de pan, integral o normal
+     * @return el BuilderPizzas
+     */
     public BuilderPizzas setTipoMasa(int tipoMasa) {
         _pizza.setTipoMasa(tipoMasa);
         return this;
     }
 
+    /**
+     * Metodo que devuelve el tamanho de la pizza
+     * @param tamanho tamaño pequeña, mediana o familiar
+     * @return el BuilderPizzas
+     */
     public BuilderPizzas setTamanho(int tamanho) {
         _pizza.setTamanho(tamanho);
         return this;
     }
 
+    /**
+     * Metodo que devuelve si hay relleno no
+     * @param relleno
+     * @return el BuilderPizzas
+     */
     public BuilderPizzas setRelleno(boolean relleno) {
         _pizza.setRelleno(relleno);
         return this;
     }
 
+    /**
+     * Metodo que devuelve si hay cebolla o no
+     * @param cebolla
+     * @return el BuilderPizza
+     */
     public BuilderPizzas setCebolla(boolean cebolla) {
         _pizza.setCebolla(cebolla);
         return this;
     }
 
+    /**
+     * Metodo que devuelve si la salsa es con gluten o no
+     * @param sinGluten
+     * @return el BuilderPizza
+     */
     public BuilderPizzas setSinGluten(boolean sinGluten) {
         _pizza.setSinGluten(sinGluten);
         return this;
     }
 
+    /**
+     * Metodo que devuelve si hay salsa o no
+     * @param salsa
+     * @return el BuilderPizza
+     */
     public BuilderPizzas setSalsa(boolean salsa) {
         _pizza.setSalsa(salsa);
         return this;
     }
 
+    /**
+     * Metodo que devuelve el tipo de salsa
+     * @param tipoSalsa salsa barbacoa, sin gluten o con solo tomamte
+     * @return
+     */
     public BuilderPizzas setTipoSalsa(int tipoSalsa) {
         _pizza.setTipoSalsa(tipoSalsa);
         return this;
     }
+
+    /**
+     * Metodo que me devuelve si habrá extra de queso
+     * @param extraQueso
+     * @return el BuilderPizza
+     */
     public BuilderPizzas setExtraQueso(boolean extraQueso) {
         _pizza.setExtraQueso(extraQueso);
         return this;
     }
+
+    /**
+     * Metodo que me devuelve si habrá pizza o no
+     * @param pinha
+     * @returnel el BuilderPizza
+     */
     public BuilderPizzas setPinha(boolean pinha) {
         _pizza.setPinha(pinha);
         return this;
     }
+
+    /**
+     * Metodo que devuelve si habra champinhones o no
+     * @param champinhones
+     * @returnel el BuilderPizza
+     */
     public BuilderPizzas setChampinhones(boolean champinhones) {
         _pizza.setChampinhones(champinhones);
         return this;
     }
+
+    /**
+     * Metodo que devuelve si habra jamon o no
+     * @param jamon
+     * @return el BuilderPizza
+     */
     public BuilderPizzas setJamon(boolean jamon) {
         _pizza.setJamon(jamon);
         return this;
     }
 
-
-
-    // public BuilderPizzas setRecojida(int recojida) {
-    //   _pizza.setRecogida(recojida);
-    // return this;
 
 
     @Override
--- src/Pizzas.java
+++ src/Pizzas.java
@@ -1,50 +1,125 @@
+/**
+ * @author Jorge Amestoy
+ * version 16.02.2023
+ */
 public class Pizzas {
+    /**
+     * Declaro el atributo tipoMasa de tipo int que me dice el tipo de masa
+     */
     private int tipoMasa;
+    /**
+     * Declaro el atributo relleno de tipo boolean que me dice si será rellena o no
+     */
     private boolean relleno;
+    /**
+     * Declaro el atributo tamañho de tipo int que me dice si será grande, media o pequeña
+     */
     private int tamanho;
+    /**
+     * Declaro el atributo salsa de tipo boolean que me dice si lleva salsa o no
+     */
     private boolean salsa;
+    /**
+     * Declaro el atributo tipoSalsa de tipo int que me dice el tipo de salsa que puede llevar
+     */
     private int tipoSalsa;
+    /**
+     * Declaro el atributo cebolla de tipo boolean que me dice si llevará cebolla o no
+     */
     private boolean cebolla;
+    /**
+     * Declaro el atributo sinGluten de tipo boolean que me dice si es con gluten o no
+     */
     private boolean sinGluten;
+    /**
+     * Declaro el atributo extraQueso de tipo boolean que me dice si llevará extra de queso o no
+     */
     private boolean extraQueso;
+    /**
+     * Declaro el atributo pinha de tipo boolean que me dice si llevará pinha o no
+     */
     private boolean pinha;
+    /**
+     * Declaro el atributo champinhones de tipo boolean que me dice si llevará champinhones o no
+     */
     private boolean champinhones;
+    /**
+     * Declaro el atributo jamon de tipo boolean que me dice si llevará jamon o no
+     */
     private boolean jamon;
     // private int recogida;
 
 
-    // tipo de masa
+    /**
+     * Declaro constante literal de tipo int que dice que el tipo de masa es fina
+     */
     public static final int FINA = 1;
+    /**
+     * Declaro constante literal de tipo int que dice que el tipo de masa es de pan
+     */
     public static final int PAN = 2;
+    /**
+     * Declaro constante literal de tipo int que dice que el tipo de masa es integral
+     */
     public static final int INTEGRAL = 3;
+    /**
+     * Declaro constante literal de tipo int que dice que el tipo de masa es normal
+     */
     public static final int NORMAL = 4;
 
-    // tamaño de la pizza
+    /**
+     * Declaro constante literal de tipo int que dice que el tamaño sera pequenho
+     */
     public static final int PEQUENHA = 1;
+    /**
+     * Declaro constante literal de tipo int que dice que el tamanho es mediano
+     */
     public static final int MEDIANA = 2;
+    /**
+     * Declaro constante literal de tipo int que dice que el tamañnho es familiar
+     */
     public static final int FAMILIAR = 3;
 
-    //tipo de salsa
+    /**
+     * Declaro constante literal de tipo int que dice que el tipo de salsa es barbacoa
+     */
     public static final int BARBACOA = 1;
+    /**
+     * Declaro constante literal de tipo int que dice que el tipo de salsa es de solo tomate
+     */
     public static final int SOLO_TOMATE = 2;
+    /**
+     * Declaro constante literal de tipo int que dice que el tipo de salsa es sin gluten
+     */
     public static final int SIN_GLUTEN = 3;
 
-    // recogida
-    //  public static final int TIENDA = 0;
-    // public static final int PARALLEVAR = 1;
-
-
-
-
+    /**
+     * Constructor por defecto
+     */
     public Pizzas() {
-        this.tipoMasa=FINA;
-        this.relleno=false;
-        this.tamanho= MEDIANA;
-        this.tipoSalsa= SOLO_TOMATE;
-        this.sinGluten=false;
-        this.jamon=true;
+        this.tipoMasa = FINA;
+        this.relleno = false;
+        this.tamanho = MEDIANA;
+        this.tipoSalsa = SOLO_TOMATE;
+        this.sinGluten = false;
+        this.jamon = true;
     }
 
+    /**
+     * Constructor parametrizado
+     *
+     * @param tipoMasa     tipo de la masa
+     * @param relleno      si va rellena o no
+     * @param tamanho      tamanho de la pizza
+     * @param salsa        si lleva salsa o no
+     * @param tipoSalsa    tipo de la salsa
+     * @param cebolla      si lleva cebolla o no
+     * @param sinGluten    si lleva gluten o no
+     * @param extraQueso   si lleva extra de que o no
+     * @param pinha        si lleva pinha o no
+     * @param champinhones si lleva champinhones o no
+     * @param jamon        si lleva jamon o no
+     */
     public Pizzas(int tipoMasa, boolean relleno, int tamanho, boolean salsa, int tipoSalsa, boolean cebolla, boolean sinGluten, boolean extraQueso, boolean pinha, boolean champinhones, boolean jamon) {
         this.tipoMasa = tipoMasa;
         this.relleno = relleno;
@@ -57,6 +132,16 @@ public class Pizzas {
         this.pinha = pinha;
         this.champinhones = champinhones;
         this.jamon = jamon;
+
+
+    }
+
+    public int getTipoMasa() {
+        return tipoMasa;
+    }
+
+    public void setTipoMasa(int tipoMasa) {
+        this.tipoMasa = tipoMasa;
     }
 
     public boolean isRelleno() {
@@ -91,6 +176,22 @@ public class Pizzas {
         this.tipoSalsa = tipoSalsa;
     }
 
+    public boolean isCebolla() {
+        return cebolla;
+    }
+
+    public void setCebolla(boolean cebolla) {
+        this.cebolla = cebolla;
+    }
+
+    public boolean isSinGluten() {
+        return sinGluten;
+    }
+
+    public void setSinGluten(boolean sinGluten) {
+        this.sinGluten = sinGluten;
+    }
+
     public boolean isExtraQueso() {
         return extraQueso;
     }
@@ -123,47 +224,19 @@ public class Pizzas {
         this.jamon = jamon;
     }
 
-    public int getTipoMasa() {
-        return tipoMasa;
-    }
-
-    public void setTipoMasa(int tipoMasa) {
-        this.tipoMasa = tipoMasa;
-    }
-
-    public boolean isCebolla() {
-        return cebolla;
-    }
-
-    public void setCebolla(boolean cebolla) {
-        this.cebolla = cebolla;
-    }
-
-    public boolean isSinGluten() {
-        return sinGluten;
-    }
-
-    public void setSinGluten(boolean sinGluten) {
-        this.sinGluten = sinGluten;
-    }
-
-
-
     @Override
     public String toString() {
-        return  "tipoMasa=" + tipoMasa +
-                ", relleno=" + relleno +
-                ", tamanho=" + tamanho +
-                ", salsa=" + salsa +
-                ", tipoSalsa=" + tipoSalsa +
-                ", cebolla=" + cebolla +
-                ", sinGluten=" + sinGluten +
-                ", extraQueso=" + extraQueso +
-                ", pinha=" + pinha +
-                ", champinhones=" + champinhones +
-                ", jamon=" + jamon;
-
+        return
+                "tipoMasa=" + tipoMasa +
+                        ", relleno=" + relleno +
+                        ", tamanho=" + tamanho +
+                        ", salsa=" + salsa +
+                        ", tipoSalsa=" + tipoSalsa +
+                        ", cebolla=" + cebolla +
+                        ", sinGluten=" + sinGluten +
+                        ", extraQueso=" + extraQueso +
+                        ", pinha=" + pinha +
+                        ", champinhones=" + champinhones +
+                        ", jamon=" + jamon;
     }
-
-
 }
